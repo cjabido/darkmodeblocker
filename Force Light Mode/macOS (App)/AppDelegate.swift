d88214c83b88ed05cef152a8d3e17893ed044efc
cjabido <cjabido@gmail.com>
2026-03-14 18:45:20 -0400

    feat: generate Xcode project via safari-web-extension-converter

---
+++ Force Light Mode/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by CArL JABIDO on 3/14/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
